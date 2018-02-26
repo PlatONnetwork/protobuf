45f58d40b06405bd4e027747017766ee5e08205d
Bo Yang <paulyang1211@gmail.com>
2018-02-26 10:16:55 -0800

    Add mingw

--- appveyor.bat
+++ appveyor.bat
@@ -1,10 +1,23 @@
 setlocal
 
+echo %PATH%
 pip install wheel
-cd %REPO_DIR%\python
+
+cd %REPO_DIR%
 git checkout %BUILD_COMMIT%
-sed -i 's/\[\'-Wno-write-strings\',/\[\]/g' setup.py
-sed -i '/Wno-invalid-offsetof/d' setup.py
-sed -i '/Wno-sign-compare/d' setup.py
-cat setup.py
-python setup.py bdist_wheel --cpp_implementation --compile_static_extension
+mingw-get
+sh autogen.sh
+
+REM cd python
+REM sed -i '/Wno-sign-compare/a \ \ \ \ extra_compile_args.append(\'-D_hypot=hypot\')' setup.py
+REM cat setup.py
+REM 
+REM REM sed -i 's/\[\'-Wno-write-strings\',/\[\]/g' setup.py
+REM REM sed -i '/Wno-invalid-offsetof/d' setup.py
+REM REM sed -i '/Wno-sign-compare/d' setup.py
+REM 
+REM dir %MINGW%
+REM set path
+REM gcc
+REM %MINGW%\gcc
+REM python setup.py bdist_wheel --cpp_implementation --compile_static_extension
