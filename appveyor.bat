dd6b670203f337bac51588370395f59c6a32df28
Bo Yang <paulyang1211@gmail.com>
2018-03-15 13:41:40 -0700

    Use msvc to compile py2.7 and py3.4 to avoid incompatibility.

--- appveyor.bat
+++ appveyor.bat
@@ -10,21 +10,6 @@ REM ======================
 
 mkdir src\.libs
 
-IF %PYTHON_VERSION%==2.7 GOTO build_core_mingw
-IF %PYTHON_VERSION%==3.4 GOTO build_core_mingw
-IF %PYTHON_VERSION%==3.5 GOTO build_core_msvc
-IF %PYTHON_VERSION%==3.6 GOTO build_core_msvc
-
-:build_core_mingw
-pushd src\.libs
-cmake -G "%generator%" -Dprotobuf_BUILD_SHARED_LIBS=%BUILD_DLL% -Dprotobuf_UNICODE=%UNICODE% -DZLIB_ROOT=%ZLIB_ROOT% -Dprotobuf_BUILD_TESTS=OFF -D"CMAKE_MAKE_PROGRAM:PATH=%MINGW%/mingw32-make.exe" ../../cmake
-mingw32-make
-dir
-SET PATH=%cd%;%PATH%
-popd
-GOTO build_core_end
-
-:build_core_msvc
 mkdir vcprojects
 pushd vcprojects
 cmake -G "%generator%" -Dprotobuf_BUILD_SHARED_LIBS=%BUILD_DLL% -Dprotobuf_UNICODE=%UNICODE% -Dprotobuf_BUILD_TESTS=OFF ../cmake
@@ -35,9 +20,6 @@ copy vcprojects\Release\libprotobuf.lib src\.libs\libprotobuf.a
 copy vcprojects\Release\libprotobuf-lite.lib src\.libs\libprotobuf-lite.a
 SET PATH=%cd%\vcprojects\Release;%PATH%
 dir vcprojects\Release
-GOTO build_core_end
-
-:build_core_end
 
 REM ======================
 REM Build python library
@@ -57,6 +39,8 @@ REM IF NOT %PYTHON_ARCH%==64 GOTO no_win64_change
 REM sed -i '/Wno-sign-compare/a \ \ \ \ extra_compile_args.append(\'-DMS_WIN64\')' setup.py
 REM sed -i 's/\'-DPYTHON_PROTO2_CPP_IMPL_V2\'/\'-DPYTHON_PROTO2_CPP_IMPL_V2\',\'-DMS_WIN64\'/g' setup.py
 REM :no_win64_change
+
+sed -i 's/\ extra_compile_args\ =\ \[\]/\ extra_compile_args\ =\ \[\'\/MT\'\]/g' setup.py
 
 REM REM MSVS default is dymanic
 REM IF NOT DEFINED vcplatform GOTO msvc_static_build_end
